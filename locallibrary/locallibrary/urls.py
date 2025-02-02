a1a360ac7c8e108cdb9772d610d80c71d53a096a
Rafael Margary <rafael.margary.junior@gmail.com>
2025-02-02 18:30:40 -0500

    Step 3

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -35,4 +35,8 @@ urlpatterns += [
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
